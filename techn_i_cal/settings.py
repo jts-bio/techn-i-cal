2f900e3f0970f24d298adf10b4476b505fc104a6
jsteinbecker <joshsteinbecker@icloud.com>
2022-12-02 02:11:56 +0000

    e

--- techn_i_cal/settings.py
+++ techn_i_cal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-rc@04_mry_3-$@2sq$b9%-9jp6q2eyxf4bsw9&&esj++aw&r)p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
         'flowrate.herokuapp.com',
